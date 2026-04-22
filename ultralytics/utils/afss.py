0ece2da8f2a7558ce499cfe6e7c4678553b37dd6
Laughing-q <1185102784@qq.com>
2026-04-22 17:32:47 +0800

    Update utils/afss.py

--- ultralytics/utils/afss.py
+++ ultralytics/utils/afss.py
@@ -66,9 +66,7 @@ class AFSSScheduler:
 
         # Easy set
         if easy_set:
-            forced_easy = [
-                i for i in easy_set if (epoch - 1 - self.state[i]["last_seen_epoch"]) >= 10
-            ]  # spec-defined formula
+            forced_easy = [i for i in easy_set if (epoch - 1 - self.state[i]["last_seen_epoch"]) >= 10]
             easy_budget = round(0.02 * len(easy_set))
             forced_easy_quota = min(len(forced_easy), math.floor(0.5 * easy_budget))
             random_easy_quota = max(easy_budget - forced_easy_quota, 0)
@@ -81,27 +79,20 @@ class AFSSScheduler:
 
                 remaining_easy = [i for i in easy_set if i not in forced_easy_sample]
                 if random_easy_quota > 0 and remaining_easy:
-                    random_easy_sample = rng.choice(
-                        remaining_easy, size=min(random_easy_quota, len(remaining_easy)), replace=False
-                    ).tolist()
+                    random_easy_sample = rng.choice(remaining_easy, size=random_easy_quota, replace=False).tolist()
                     selected.extend(random_easy_sample)
 
         # Moderate set
         if moderate_set:
-            forced_moderate = [
-                i
-                for i in moderate_set
-                if (epoch - 1 - self.state[i]["last_seen_epoch"]) >= 3  # spec-defined formula
-            ]
+            forced_moderate = [i for i in moderate_set if (epoch - 1 - self.state[i]["last_seen_epoch"]) >= 3]
             M1 = round(0.4 * len(moderate_set)) - len(forced_moderate)
             random_moderate_quota = max(min(len(moderate_set) - len(forced_moderate), M1), 0)
-
             selected.extend(forced_moderate)
 
             remaining_moderate = [i for i in moderate_set if i not in forced_moderate]
             if random_moderate_quota > 0 and remaining_moderate:
                 random_moderate_sample = rng.choice(
-                    remaining_moderate, size=min(random_moderate_quota, len(remaining_moderate)), replace=False
+                    remaining_moderate, size=random_moderate_quota, replace=False
                 ).tolist()
                 selected.extend(random_moderate_sample)
 
