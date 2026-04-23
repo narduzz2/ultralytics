1fc0c67ac81199d6401600e157ae62679d47096e
Onuralp SEZER <onuralp@ultralytics.com>
2026-04-23 03:19:31 +0300

    Add FastTracker to the tracker module exports
    Signed-off-by: Onuralp SEZER <onuralp@ultralytics.com>

--- ultralytics/trackers/__init__.py
+++ ultralytics/trackers/__init__.py
@@ -2,6 +2,7 @@
 
 from .bot_sort import BOTSORT
 from .byte_tracker import BYTETracker
+from .fast_tracker import FastTracker
 from .track import register_tracker
 
-__all__ = "BOTSORT", "BYTETracker", "register_tracker"  # allow simpler import
+__all__ = "BOTSORT", "BYTETracker", "FastTracker", "register_tracker"  # allow simpler import
