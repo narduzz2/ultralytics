fcb43ebf48f00fbfb4f5ef93f3ed912d3edd6f23
Onuralp SEZER <onuralp@ultralytics.com>
2026-05-01 16:32:15 +0300

    fix: simplify return statement in BOTSORT
    Signed-off-by: Onuralp SEZER <onuralp@ultralytics.com>

--- ultralytics/trackers/bot_sort.py
+++ ultralytics/trackers/bot_sort.py
@@ -205,8 +205,7 @@ class BOTSORT(BYTETracker):
         if self.args.with_reid and self.encoder is not None:
             features_keep = self.encoder(img, bboxes)
             return [BOTrack(xywh, s, c, f) for (xywh, s, c, f) in zip(bboxes, results.conf, results.cls, features_keep)]
-        else:
-            return [BOTrack(xywh, s, c) for (xywh, s, c) in zip(bboxes, results.conf, results.cls)]
+        return [BOTrack(xywh, s, c) for (xywh, s, c) in zip(bboxes, results.conf, results.cls)]
 
     def get_dists(self, tracks: list[BOTrack], detections: list[BOTrack]) -> np.ndarray:
         """Calculate distances between tracks and detections using IoU and optionally ReID embeddings."""
